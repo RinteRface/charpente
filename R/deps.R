1b36b7316bd800d50b15aa2d91e8e29969b6199a
DivadNojnarg <dgranjon@ymail.com>
2020-09-04 02:02:21 +0200

    - add get_dependency_versions - add get_dependency_assets - add file_create - add select_asset helper

--- R/deps.R
+++ R/deps.R
@@ -1,37 +1,68 @@
 #' Imports External Dependencies
 #'
-#' Download and create depdendency function.
+#' Download and create dependency function.
 #'
 #' @param name Name of library.
-#' @param scripts,stylesheets Vector of JavaScript and CSS files to use
-#' as depdendencies.
+#' @param local Whether to download files locally or to point to a CDN. Default to TRUE.
+#' @param version Library version. Default to NULL. If NULL, takes the latest version.
 #'
 #' @export
-create_dependency <- function(name, scripts = NULL, stylesheets = NULL){
+create_dependency <- function(name, local = TRUE, version = NULL){
 
   # assert is in package
   # should further check valid name, e.g.: no spaces or punctuation
 
   # checks
   if(missing(name)) stop("Missing `name`")
-  if(is.null(scripts) && is.null(stylesheets))
-    stop("Must have at least one of `scripts` or `stylesheets`")
 
-  # path to dependency
-  path <- sprintf("inst/%s", name)
+  # discover assets and stop if not
+  if (is.null(version)) version <- get_dependency_versions(name, TRUE)
+  assets <- get_dependency_assets(name, version)
+  if(nrow(assets$files) == 0) stop(sprintf("No assets found for %s", name))
 
-  # create directories
-  if(!is.null(scripts)) directory_create_asset(path, "js")
-  if(!is.null(stylesheets)) directory_create_asset(path, "css")
 
-  # download dependencies
-  dependencies_download(path, scripts)
-  dependencies_download(path, stylesheets)
+
+  # if local download files and create directories
+  if (local) {
+    # path to dependency
+    path <- sprintf("inst/%s-%s", name, version)
+
+    scripts <- select_asset(assets$files, "js", name)
+    stylesheets <- select_asset(assets$files, "css", name)
+
+    if(!is.null(scripts)) directory_create_asset(path, "js")
+    if(!is.null(stylesheets)) directory_create_asset(path, "css")
+
+    # download dependencies
+    dependencies_download(path, paste0(assets$url, scripts))
+    dependencies_download(path, paste0(assets$url, stylesheets))
+  }
+
+  # TO DO ... create html_dependencies.R
+  #file_create(path = "")
+
 }
+
+
+
+#' Select a specific type of asset
+#'
+#' @param name Library name.
+#' @param assets Dataframe of assets.
+#' @param type Type to extract. Could be js, css, ...
+#'
+#' @return A vector
+#' @noRd
+#' @keywords internal
+select_asset <- function(assets, type, name) {
+  selected_assets <- assets[grep(sprintf("^%s.(bundle).min.%s$", name, type), assets$name), ]$name
+  paste(type, selected_assets, sep = "/")
+}
+
 
 #' Creates Assets Directories
 #'
-#' @param path Path to store depdendencies `inst/libraryName`.
+#' @param path Path to store dependencies `inst/libraryName`.
 #' @param asset Asset path to create, depends on file type.
 #'
 #' @noRd
@@ -45,7 +76,7 @@ directory_create_asset <- function(path, asset = c("js", "css")){
 #' Download Dependencies
 #'
 #' @inheritParams directory_create_asset
-#' @param files Files to download.
+#' @param files Files to download
 #'
 #' @noRd
 #' @keywords internal
@@ -69,48 +100,94 @@ dependencies_download <- function(path, files){
 }
 
 
-options("CDN" = "https://api.cdnjs.com/libraries/")
+options("DEFAULT_CDN" = "https://data.jsdelivr.com/v1/package/npm/")
 #' Get all version for the current dependency
 #'
+#' Query from \url{https://data.jsdelivr.com/v1/package/npm/} under the hood.
+#'
 #' @param dep Library name.
-#' @param cdn CDN url. Default to \url{https://api.cdnjs.com}.
 #' @param latest Whether to get the last version. Default to FALSE.
 #'
 #' @return A vector containing all versions
 #' @export
 #'
 #' @examples
-#' get_dependency_versions("framework7")
-#' get_dependency_versions("bootstrap")
-#' get_dependency_versions("react", latest = TRUE)
-get_dependency_versions <- function(dep, cdn = NULL, latest = FALSE) {
+#' \dontrun{
+#'  get_dependency_versions("framework7")
+#'  get_dependency_versions("bootstrap")
+#'  get_dependency_versions("react", latest = TRUE)
+#' }
+get_dependency_versions <- function(dep, latest = FALSE) {
   # default to cdnjs
-  cdn <- if (is.null(cdn)) getOption("CDN")
+  cdn <- getOption("DEFAULT_CDN")
   tryCatch({
-    cli::cli_alert_info("Trying with cdnjs")
-    res <- jsonlite::fromJSON(sprintf("%s%s", cdn, dep))$versions
+    cli::cli_alert_info(sprintf("Trying with %s%s", cdn, dep))
+    temp <- jsonlite::fromJSON(sprintf("%s%s", cdn, dep))
     if (latest) {
       cli::cli_alert_success("Success!")
       cli::cli_rule()
-      res[length(res)]
+      temp$tags$latest
     } else {
       cli::cli_alert_success("Success!")
       cli::cli_rule()
-      res
+      temp$versions
     }
   }, error = function(e) {
-    cli::cli_alert_danger("Failed with cdnjs")
-    cli::cli_alert_info("Trying with jsdelivr")
-    tryCatch({
-      cli::cli_alert_success("Success!")
-      cli::cli_rule()
-      temp <- jsonlite::fromJSON(sprintf("https://data.jsdelivr.com/v1/package/npm/%s", dep))
-      if (latest) temp$tags$latest else temp$versions
-    }, error = function(e) {
-      cli::cli_alert_danger(sprintf("Failed: we tried from cdnjs and jsdelivr but %s", e))
-    })
+    cli::cli_alert_danger(sprintf("Failed: we tried but %s", e))
   })
 }
 
 
-#https://api.cdnjs.com/libraries/framework7/5.7.12?fields=files,sri
+
+#' Get all links to dependencies
+#'
+#' Query from \url{https://data.jsdelivr.com/v1/package/npm/} under the hood.
+#'
+#' @param dep Library name.
+#' @param version Library version. Default to latest.
+#'
+#' @return A list of url containing links to CSS and JS dependencies for the given library.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'  get_dependency_assets("bootstrap")
+#'  get_dependency_assets("framework7", version = "5.5.5")
+#' }
+get_dependency_assets <- function(dep, version = "latest") {
+  # get all js and css assets from jsdelivr
+  cdn <- getOption("DEFAULT_CDN")
+  if (version == "latest") version <- get_dependency_versions(dep, TRUE)
+  temp <- jsonlite::fromJSON(sprintf("%s%s@%s", cdn, dep, version))$files
+
+  download_endpoint <- "https://cdn.jsdelivr.net/npm/"
+  url <- sprintf("%s%s@%s/", download_endpoint, dep, version)
+
+  # bootstrap has a dist folder
+  if ("dist" %in% temp$name) {
+    temp <- temp[temp$name %in% c("dist"), "files"][[1]]
+    temp <- do.call(rbind, temp$files)
+    list(url = paste0(url, "dist/"), files = temp[, c("name", "hash")])
+  } else {
+    temp <- temp[temp$name %in% c("css", "js"), "files"]
+    temp <- do.call(rbind, temp)
+    list(url = url, files = temp[, c("name", "hash")])
+  }
+}
+
+
+
+#fromJSON("https://data.jsdelivr.com/v1/package/npm/framework7@5.7.12") %>%
+#  tibble::as_tibble() %>%
+#  dplyr::filter(files$name %in% c("css", "js")) %>%
+#  dplyr::mutate(
+#    files = files$files,
+#    name = files$name
+#  ) %>%
+#  dplyr::select(files) %>%
+#  tidyr::unnest(c(files)) %>%
+#  dplyr::select(name, hash)
+
+
+
+
--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,8 @@
 #' Create Directory
-#' 
+#'
 #' @param path Path to directory to create.
 #' @param ... Addition arguments to pass to [fs::dir_create()]
-#' @noRd 
+#' @noRd
 #' @keywords internal
 directory_create <- function(path, ...){
 
@@ -13,4 +13,25 @@ directory_create <- function(path, ...){
 
   fs::dir_create(path, ...)
   cli::cli_alert_success("Directory {.file {path}} successfully created")
+}
+
+
+
+#' Create file
+#'
+#' @param path Path to file.
+#' @param ... Elements to add.
+#' @noRd
+#' @keywords internal
+file_create <- function(path, ...){
+
+  exists <- fs::file_exists(paste(path, ..., sep ="/"))
+  if(exists) {
+    cli::cli_alert_warning("Editing file at {.file {path}}")
+    write(..., file = path, append = TRUE)
+  } else {
+    fs::file_create(path)
+    write(..., file = path)
+    cli::cli_alert_success("File {.file {path}} successfully created")
+  }
 }
