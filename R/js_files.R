221e7b95c8e88080c2eb9d2e2bb87a266da60f49
DivadNojnarg <dgranjon@ymail.com>
2020-09-21 18:08:07 +0200

    import from {golem}

---
+++ R/js_files.R
@@ -0,0 +1,23 @@
+#' @inheritParams golem::add_js_input_binding
+#' @export
+create_input_binding <- golem::add_js_input_binding
+
+
+#' @inheritParams golem::add_js_output_binding
+#' @export
+create_output_binding <- golem::add_js_output_binding
+
+
+#' @inheritParams golem::add_js_handler
+#' @export
+create_custom_handler <- golem::add_js_handler
+
+
+#' @inheritParams golem::add_js_file
+#' @export
+create_js <- golem::add_js_file
+
+
+#' @inheritParams golem::add_css_file
+#' @export
+create_css <- golem::add_css_file
